77d4dfeb0c30716b52ea7cf7c959cbe2e99d0e97
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-25 00:27:57 +0530

    understanding propes

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,16 +2,17 @@
 import "./Product.css";
 
 
-function Product(){
+function Product({title,price=1}){  // props as arguement with default values..!!
 return (
 <div className="Product">
-    <h3>I am a product</h3>
-    <h5>this is description</h5>
+    <h3>{title}</h3>
+    <h5>Price:{price}</h5>
 </div>
 
 );
 
 }
+
 
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -3,9 +3,10 @@ function ProductTab(){
 
 return (
     <>
-    <Product/>
-    <Product/>
-    <Product/>
+    <Product title="phone" price="20000"/>
+    <Product title="laptop" price={50000}/>  
+    {/* {2000} here price is treated as number..!! */}
+    <Product title="pen"/>
     </>
 
 );
