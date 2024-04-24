2acafdc5e4f86d278a9efe9d83e1df72c012b468
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-24 16:47:57 +0530

    basic react/vite setup

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+import './App.css'
+
+function App() {  // this is our app component..!!
+ 
+
+  return <button>submit</button>  // whatever is written after return will be rendered and dislpayed on your screen
+}
+
+export default App
