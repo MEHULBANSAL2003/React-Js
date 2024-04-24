1f013b7de6c6c5760d52ed6f60496954717026bc
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-24 16:56:09 +0530

    creating our first component in app.jsx itself

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-
 import './App.css'
+
+//creating 1st component..
+function Title(){
+  return <h1>I am the title</h1>
+}
+
+function Description(){
+  return <h3>I am the description</h3>
+}
 
 function App() {  // this is our app component..!!
  
 
-  return <button>submit</button>  // whatever is written after return will be rendered and dislpayed on your screen
+//  return <button>submit</button>  // whatever is written after return will be rendered and dislpayed on your screen
+
+return( 
+<div>
+<Title/>
+<Description/>
+<Title></Title>
+<Description></Description>
+
+
+ </div>
+);
 }
 
 export default App
