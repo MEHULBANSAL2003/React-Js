379e09789c3da70e54470a7d28789e57f3e2f5b1
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-24 17:48:28 +0530

    styling react component in different .css file of same name as of jsx and importing it

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,12 @@
+// directly importing product.css using webpack that allows us to do so 
+import "./Product.css";
+
+
 function Product(){
 return (
-<div>
-    <h1>I am a product</h1>
-    <h3>this is description</h3>
+<div className="Product">
+    <h3>I am a product</h3>
+    <h5>this is description</h5>
 </div>
 
 );
