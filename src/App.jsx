b04cc07eb2d498aee2ddcf196c369066e009720f
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-24 17:13:41 +0530

    returning app component with <> </> tag (react fragment)

--- src/App.jsx
+++ src/App.jsx
@@ -13,14 +13,14 @@ function App() {  // this is our app component..!!
 //  return <button>submit</button>  // whatever is written after return will be rendered and dislpayed on your screen
 
 return( 
-<div>
+<>
 <Title/>
 <Description/>
 <Title></Title>
 <Description></Description>
 
-
- </div>
+</>
+ 
 );
 }
 
