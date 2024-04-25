9836ad73047226004db7dc95fc64cf71fac6bd73
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-25 22:22:46 +0530

    rendering array as a list by passing it as a list tag

--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,11 +1,11 @@
 import Product from "./Product.jsx"
 function ProductTab(){
-    //let options=["hi-tech","durable","fast"];
+    let options=[<li>"hi-tech"</li>,<li>"durable"</li>,<li>"fast"</li>];
  
 
 return (
     <>
-    <Product title="phone" price={20000} features={["hi-tech","durable"]}/>
+    <Product title="phone" price={20000} features={options}/>
     <Product title="laptop" price={50000}/>  
     {/* {2000} here price is treated as number..!! */}
     <Product title="pen" price={10}/>
