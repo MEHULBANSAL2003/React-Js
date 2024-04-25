24ce3d0f12473eccdc15681240097e314c1a3722
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-26 00:11:57 +0530

    other syntax of doing same using && operator

--- src/Product.jsx
+++ src/Product.jsx
@@ -6,14 +6,14 @@ import PropTypes from "prop-types";
 function Product({title,price,features=[]}){  // props as arguement with default values..!!
   const list = features.map((feature, index) => <li key={index}>{feature}</li>);
 
-  let discount=(price>30000)?"Discount: 5%":"";
+  //let discount=(price>30000)?"Discount: 5%":"";
 
 //console.log(list)
 return (
 <div className="Product">
     <h3>{title}</h3> 
     <h5>Price:{price}</h5>
-    <h5>{discount}</h5>
+    <h5>{(price>30000)&&(<p>Discount of 5%</p>)}</h5>
     <p>{list}</p>
 
 </div>
