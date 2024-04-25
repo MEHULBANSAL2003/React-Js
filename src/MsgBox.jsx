ba065978caa69867ba1dae4384e4be3546e3095d
Mehul Bansal <mehulbansal83606@gmail.com>
2024-04-26 01:39:01 +0530

    .

--- src/MsgBox.jsx
+++ src/MsgBox.jsx
@@ -1,17 +1,11 @@
 import PropTypes from "prop-types";
 
-export default function MsgBox({username,textColor}){
-   let styles={color:textColor}
-    return(
-        <h1 style={styles}>Hello {username}</h1>
-    );
-
+export default function MsgBox({ username, textColor }) {
+  let styles = { color: textColor };
+  return <h1 style={styles}>Hello {username}</h1>;
 }
 
 MsgBox.propTypes = {
-    username: PropTypes.string.isRequired, // Require 'title' prop as a string
-    textColor: PropTypes.string, // Allow 'price' prop as a number, but it's optional
-    
-    
- 
-  };
+  username: PropTypes.string.isRequired, // Require 'title' prop as a string
+  textColor: PropTypes.string, // Allow 'price' prop as a number, but it's optional
+};
